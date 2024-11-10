4239728e42f7f63a4af9ee0523d7f021fd2215e3
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-11-10 10:27:36 +0530

    Expandablle Slider

---
+++ SwiftUiPractice/Helpers/CustomSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSlider.swift
+//  SwiftUiPractice
+//
+//  Created by Mukthar Amiyan on 09/11/24.
+//
+
+import SwiftUI
+
+struct CustomSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSlider()
+}
