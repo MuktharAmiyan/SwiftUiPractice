e462f983e1609f0f7f48fa17930c06cc1409d2ab
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-11-10 10:27:52 +0530

    Expandable slider

--- SwiftUiPractice/ContentView.swift
+++ SwiftUiPractice/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var volume: CGFloat = 30
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                CustomSlider(value: $volume, in: 10...100) {
+                    HStack {
+                        Image(
+                            systemName: "speaker.wave.3.fill",
+                            variableValue: volume / 100
+                        )
+                        
+                        Spacer(minLength: 0)
+                        
+                        Text(String(format: "%.1f", volume) + "%")
+                            .font(.callout)
+                    }
+                    .padding(.horizontal,20)
+                }
+                
+                Slider(value: $volume, in: 10...100)
+            }
+            .padding(15)
+            .navigationTitle("Expandable Slider")
         }
-        .padding()
     }
 }
 
